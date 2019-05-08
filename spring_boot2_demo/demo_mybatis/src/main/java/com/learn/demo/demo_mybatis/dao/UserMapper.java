d39ecf26f32d25024d7a20e7b7b7b2f627c0bf05
xiaozhupig <xuexizw@126.com>
2019-05-08 14:11:20 +0800

    mybatis-generator 小项目

---
+++ spring_boot2_demo/demo_mybatis/src/main/java/com/learn/demo/demo_mybatis/dao/UserMapper.java
@@ -0,0 +1,17 @@
+package com.learn.demo.demo_mybatis.dao;
+
+import com.learn.demo.demo_mybatis.pojo.User;
+
+public interface UserMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(User record);
+
+    int insertSelective(User record);
+
+    User selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(User record);
+
+    int updateByPrimaryKey(User record);
+}
---
+++ spring_boot2_demo/demo_mybatis/src/main/java/com/learn/demo/demo_mybatis/pojo/User.java
@@ -0,0 +1,65 @@
+package com.learn.demo.demo_mybatis.pojo;
+
+public class User {
+    private Integer id;
+
+    private String name;
+
+    private Integer age;
+
+    private Long phone;
+
+    private String email;
+
+    public User(Integer id, String name, Integer age, Long phone, String email) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.phone = phone;
+        this.email = email;
+    }
+
+    public User() {
+        super();
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name == null ? null : name.trim();
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public Long getPhone() {
+        return phone;
+    }
+
+    public void setPhone(Long phone) {
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email == null ? null : email.trim();
+    }
+}
